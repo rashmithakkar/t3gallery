395ce80bf6f1977da8653c264cee9be639972fc7
Rath0521 <rashmi.h.thakkar@netcracker.com>
2025-01-26 22:10:35 +0530

    Use auth for homepage

--- README.md
+++ README.md
@@ -9,6 +9,7 @@
 - [x] Attach database to UI
 - [x] Add Authentication (w/ clerk)
 - [ ] Add Image upload
+- [ ] "taint" (server-only)
 - [ ] Error Management (w/ Sentry)
 - [ ] Routing/image page (parallel route)
 - [ ] Delete button (w/ Server Actions)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { db } from "~/server/db";
 
@@ -23,23 +24,34 @@ export const dynamic = "force-dynamic";
 //   url,
 // }));
 
-export default async function HomePage() {
-const images = await db.query.images.findMany(
+async function Images() {
+  const images = await db.query.images.findMany(
   {
     orderBy:(model, { desc }) => desc(model.id),
   }
 );
+return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image, index) => (
+        <div key={image.id + '-' + index} className="w-48 flex-col">
+          <img src={image.url} />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default async function HomePage() {
 
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {images.map((image, index) => (
-          <div key={image.id + '-' + index} className="w-48 flex-col">
-            <img src={image.url} />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-2xl text-center">Please Sign In above</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
