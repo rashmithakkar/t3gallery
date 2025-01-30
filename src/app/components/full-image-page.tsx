5e5eaf6b6c4ef77935ff754763aea3ebdf4a6fc1
Rath0521 <rashmi.h.thakkar@netcracker.com>
2025-01-30 12:37:22 +0530

    Analytics on delete button

--- src/app/components/full-image-page.tsx
+++ src/app/components/full-image-page.tsx
@@ -1,31 +1,43 @@
 import { clerkClient } from "@clerk/nextjs/server";
-import { getImage } from "~/server/queries";
+import { Button } from "~/components/ui/button";
+import { deleteImage, getImage } from "~/server/queries";
 
-export default async function FullPageImageView(props: {id: number}) {
+export default async function FullPageImageView(props: { id: number }) {
 
-  const image = await getImage(props.id);
+    const image = await getImage(props.id);
 
-  const uploaderInfo = await (await clerkClient()).users.getUser(image.userId); 
+    const uploaderInfo = await (await clerkClient()).users.getUser(image.userId);
 
-  return (
-    <div className="flex w-full h-full min-w-0">
-        <div className="flex-shrink flex items-center justify-center">
-            <img className="flex-shrink object-contain" src={image.url} alt={image.name}/>
-        </div>  
-        
-        <div className="w-48 flex flex-col flex-shrink-0 border-l">
-            <div className="text-lg border-b text-center p-2">{image.name}</div>
-            
-            <div className="flex flex-col p-2">
-                <span>Uploaded By: </span>
-                <span>{uploaderInfo.fullName}</span>
+    return (
+        <div className="flex w-full h-full min-w-0">
+            <div className="flex-shrink flex items-center justify-center">
+                <img className="flex-shrink object-contain" src={image.url} alt={image.name} />
             </div>
 
-            <div className="flex flex-col p-2">
-                <span>Created On: </span>
-                <span>{new Date(image.createdAt).toLocaleDateString()}</span>
+            <div className="w-48 flex flex-col flex-shrink-0 border-l">
+                <div className="text-lg border-b text-center p-2">{image.name}</div>
+
+                <div className="flex flex-col p-2">
+                    <span>Uploaded By: </span>
+                    <span>{uploaderInfo.fullName}</span>
+                </div>
+
+                <div className="flex flex-col p-2">
+                    <span>Created On: </span>
+                    <span>{new Date(image.createdAt).toLocaleDateString()}</span>
+                </div>
+
+                <div className="p-2">
+                <form action={async () => {
+                    "use server";
+                    await deleteImage(image.id);
+                }}>
+                    <Button type="submit" variant="destructive">
+                        Delete
+                    </Button>
+                </form>
+                </div>
             </div>
         </div>
-    </div>
-  ) 
+    )
 }
--- src/server/queries.ts
+++ src/server/queries.ts
@@ -1,32 +1,54 @@
 import "server-only";
 import { db } from "./db";
 import { auth } from "@clerk/nextjs/server";
+import { images } from "./db/schema";
+import { and, eq } from "drizzle-orm";
+import { redirect } from "next/navigation";
+import analyticsServerClient from "./db/analytics";
 
 export async function getMyImages() {
-    const user = await auth();
+  const user = await auth();
 
-if (!user.userId) throw new Error("Unauthorized");
+  if (!user.userId) throw new Error("Unauthorized");
 
-    const images = await db.query.images.findMany(
-        { 
-          where: (model, { eq }) => eq(model.userId, user.userId),
-          orderBy:(model, { desc }) => desc(model.id),
-        }
-      );
-      return images;
-} 
+  const images = await db.query.images.findMany(
+    {
+      where: (model, { eq }) => eq(model.userId, user.userId),
+      orderBy: (model, { desc }) => desc(model.id),
+    }
+  );
+  return images;
+}
 
 export async function getImage(id: number) {
   const user = await auth();
-  if(!user.userId) throw new Error("Unauthorized");
+  if (!user.userId) throw new Error("Unauthorized");
 
   const image = await db.query.images.findFirst({
-    where: (model, {eq}) => eq(model.id, id),
+    where: (model, { eq }) => eq(model.id, id),
   });
 
-  if(!image) throw new Error("Image not found");
+  if (!image) throw new Error("Image not found");
 
-  if(image.userId !== user.userId) throw new Error("Unauthorized");
-  
+  if (image.userId !== user.userId) throw new Error("Unauthorized");
+
   return image;
+}
+
+export async function deleteImage(id: number) {
+  const user = await auth();
+  if (!user.userId) throw new Error("Unauthorized");
+
+  await db
+    .delete(images)
+    .where(and(eq(images.id, id), eq(images.userId, user.userId)));
+
+  analyticsServerClient.capture({
+    distinctId: user.userId,
+    event: "delete image",
+    properties: {
+      imageId: id,
+    },
+  });
+  redirect("/");
 }
